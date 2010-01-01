65550ee6a64ba6abd56bc108a8c442002fedb384
Michael Budde <mbudde@gmail.com>
2010-01-01 14:17:02 +0100

    Add tests for Config

---
+++ tests/TestConfig.py
@@ -0,0 +1,43 @@
+
+import unittest
+import gtk
+import gconf
+from GmailNotifier.Config import Config
+
+GCONF_PATH = '/apps/gmail-notifier-test'
+
+class TestKeyring(unittest.TestCase):
+
+    props = (
+        ('notifications', True),
+        ('notification-mode', 'count'),
+        ('run-on-startup', False),
+        ('mail-application', 'browser'),
+        ('custom-app-name', ''),
+        ('custom-app-icon', ''),
+        ('custom-app-exec', ''),
+        ('custom-app-terminal', False)
+    )
+
+    def setUp(self):
+        self.conf = Config(GCONF_PATH)
+        self.gconf = gconf.client_get_default()
+
+    def test_getters(self):
+        for prop, _ in self.props:
+            self.assertEqual(self.conf.get_property(prop),
+                             getattr(self.conf.props, prop))
+
+    def test_defaults(self):
+        for prop, val in self.props:
+            self.assertEqual(self.conf.get_property(prop), val)
+
+    def test_get_accounts(self):
+        self.assertEqual(self.conf.get_accounts(), [])
+
+    def tearDown(self):
+        self.gconf.recursive_unset(GCONF_PATH, 1)
+        self.gconf.suggest_sync()
+
+if __name__ == '__main__':
+    unittest.main()
