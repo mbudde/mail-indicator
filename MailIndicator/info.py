9418ea26940a90aa182a4ed21fd38c2103e47dcd
Michael Budde <mbudde@gmail.com>
2010-01-03 14:53:49 +0100

    Update info

--- MailIndicator/info.py
+++ MailIndicator/info.py
@@ -1,24 +1,9 @@
-#
-# Copyright (C) 2009  Michael Budde <mbudde@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 APPNAME      = 'mail-indicator'
 NAME         = 'Mail Indicator'
-VERSION      = '1.0-alpha'
-DESCRIPTION  = ''
-WEBSITE      = ''
+VERSION      = '0.3'
+DESCRIPTION  = 'Get notifications for new mails in your mail accounts'
+WEBSITE      = 'http://launchpad.net/mail-indicator'
 AUTHORS = [
     'Michael Budde <mbudde@gmail.com>',
 ]
